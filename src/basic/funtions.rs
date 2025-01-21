132e043a8940522b605b12cd95cc4694a5a7ffd5
rencels <rencels@gmail.com>
2025-01-21 16:24:51 +0100

    Added the functions

--- src/basic/funtions.rs
+++ src/basic/funtions.rs
@@ -1,4 +1,5 @@
 //Name, Parameters, Return type, body - Named function
+//1 Named functions
 pub fn anatomy(a:i32, b:i32) -> i32 {
 
     if a > 10 {
@@ -8,17 +9,46 @@ pub fn anatomy(a:i32, b:i32) -> i32 {
     a + b
 }
 
+//2. Annonymous functions(closures) -> do not have a nice, special syntax and can capture outside environment
 pub fn test_annonymous(a:i32, b:i32) -> i32 {
 
-    let my_anon_fn = |a: i32, b: i32| if a > b { a + b } else { a - b };
+    let my_num = 42; //we can capture this value from the closuse
+
+    let my_anon_fn = |a: i32, b: i32| if a > my_num { a + b } else { a - b };
 
     my_anon_fn(a , b)    
 }
 
-//Named functions
 
-//Annonymous functions(closures) -> do not have a nice, special syntax and can capture outside environment
 
-//Methods on the instances of the type(struct)
+
+
+//3 Methods on the instances of the type(struct) - Function Associated With a Type
+#[derive(Debug)]
+pub struct Point {
+  pub X: i32,
+  pub Y: i32    
+}
+
+impl Point {
+    pub fn new(x: i32, y: i32) -> Point
+    {
+        Point {
+            X: x,
+            Y: y
+         }
+    }
+
+    pub fn print(&self)
+    {
+        println!("X:{} Y:{}", self.X, self.Y);
+    }
+    
+    pub fn add(&mut self, new_value: i32)
+    {
+        self.X += new_value;
+        self.Y += new_value;
+    }
+}
 
 //Associated funtions -> similar to static methods You do not need an instance
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ mod basic {
     pub mod control_flow;
     pub mod funtions;
 }
+
 use basic::ownership_and_borrowing;
 use crate::basic::prints;
 use crate::basic::control_flow;
 use crate::basic::funtions;
+use basic::funtions::Point;
 
 fn main() {   
     
@@ -17,23 +19,30 @@ fn main() {
     //println!("Start owneship_and_borrowing!");
     //ownership_and_borrowing::owneship_and_borrowing();
 
-    println!("Control flow!");
-    let can_he_vote = control_flow::can_vote(18);
-    println!("Can he vote at the age of 18? {}!", can_he_vote);
+    //println!("Control flow!");
+    //let can_he_vote = control_flow::can_vote(18);
+    //println!("Can he vote at the age of 18? {}!", can_he_vote);
 
-    let loop_value = control_flow::try_loop();
-    println!("Loop value? {}!", loop_value);
+    //let loop_value = control_flow::try_loop();
+    //println!("Loop value? {}!", loop_value);
 
-    let while_value = control_flow::try_while();
-    println!("While value: {}!", while_value);
+    //let while_value = control_flow::try_while();
+    //println!("While value: {}!", while_value);
 
-    control_flow::try_pattern_matching(12);  
+    //control_flow::try_pattern_matching(12);  
 
     let sum_result = funtions::anatomy(11, 2);
     println!("result: {}!", sum_result);
 
-    let test_annonymous_result = funtions::test_annonymous(11, 2);
+    let test_annonymous_result = funtions::test_annonymous(43, 2);
     println!("annonymous_result: {}!", test_annonymous_result);
+
+    let mut new_point = Point::new(10, 5);
+     
+    new_point.print();
+    new_point.add(7);
+    new_point.print();
+
 }
 
 
